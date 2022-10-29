6e583b77682a28871b3dab65fedfa84e47961acf
JazibEqbal <jazibeqbal0526@gmail.com>
2022-10-30 00:21:09 +0530

    recent

--- src/Screen/Recent.js
+++ src/Screen/Recent.js
@@ -2,8 +2,14 @@ import React from "react";
 import styles from "./Recent.module.css";
 function Recent() {
   return (
-    <div className={styles.container}>
-      Recent
+    <div className={styles.mainDiv}>
+      <div className={styles.recentContainer}>
+        <div className={styles.recentContents}>
+          {/* <h1>hdhdhd</h1> */}
+          <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="img" />
+          
+        </div>
+      </div>
     </div>
   );
 }
