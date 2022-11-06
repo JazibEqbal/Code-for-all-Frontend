f52522ab9203215c31dd2db1540d38309052749e
JazibEqbal <jazibeqbal0526@gmail.com>
2022-11-06 19:55:26 +0530

    changes

--- src/Screen/Contests.js
+++ src/Screen/Contests.js
@@ -15,7 +15,7 @@ function Contests() {
     }
     getData();
   }, []);
-  console.log(process.env.REACT_APP_BACKEND_URL);
+  //console.log(process.env.REACT_APP_BACKEND_URL);
   const renderContests = response.map(
     item =>
       !item.ongoing
